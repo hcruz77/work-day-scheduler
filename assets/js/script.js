05c87a850f9d1e6853a332308dddd65ce28a7c28
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-11-28 16:52:39 -0500

    fixed js to change color during current time, removed duplicate code, updated html, updated readme screenshot

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,37 @@
+var currentTime = moment().hour();
+
+$('#currentDay').text(moment($("#date_val").val()).format('dddd, MMMM Do YYYY, h:mmA'));
 
 
-$('#currentDay').text(moment($("#date_val").val()).format('dddd, MM Do YYYY, h:mmA'));
-
-$
-
-$('.container').on('click', '.detail', function() {
-    var parentId = $(this).parent().attr('data-id');
-    var detail = $(this).val();
-    localStorage.setItem(parentId, detail);
-  });
-
-
-
- $('.container').on('click', '.done', function() {
-    var parentId = $(this).parent().attr('data-id');
+ $('.container').on('click', 'button', function() {
+    var time = $(this).parent().attr('data-id');
     var detail = $(this).prev().val();
-    localStorage.setItem(parentId, detail);
+    localStorage.setItem(time, detail);
   });
 
   $('.detail').each(function() {
-    var parentId = $(this).parent().attr('data-id');
-     var detail = localStorage.getItem(parentId);
+    var time = $(this).parent().attr('data-id');
+     var detail = localStorage.getItem(time);
      $(this).val(detail);  
     
+     if (time == currentTime){
+     $(this).addClass('bg-danger');
+     }  if(time < currentTime){
+      $(this).addClass('bg-secondary');
+       } if (time > currentTime) {
+       $(this).addClass('bg-success');
+       }
  });
     
- var now = moment();
- if ('#currentDay' === now){
-  textAreaEl.setAttribute("background-color: red");
- } else if('#currentDay' < now){
-  textAreaEl.setAttribute("background-color: gray");
-   } else ('#currentDay' > now)
-    textAreaEl.setAttribute("background-color: green");
 
 
 
-//need function to get color coding to time of day
-    
-  //var textAreaEl = $(".detail")
-  // var now = moment();
-//
-  // for(var i = 9; i < textAreaEl.length; i++){
-  //  if (moment()=== now) {
-  //  textAreaEl[i].setAttribute("background-color: gray") 
-//
-  // }
 
 
 
-  // if time is present row = red
-   // if time is past row = gray
-   // if time is future row = green
+
+
+
 
 
   
